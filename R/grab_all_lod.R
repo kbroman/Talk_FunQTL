697eecb74c2e5c00796a2980aba6cc1a41aeb9af
Karl Broman <kbroman@gmail.com>
2013-03-17 23:08:33 -0500

    Changes re data grabbed in grab_all_lod.R

--- R/grab_all_lod.R
+++ R/grab_all_lod.R
@@ -13,17 +13,6 @@ load("out.RData")
 # times (in minutes)
 times <- as.numeric(substr(phenames(spal), 2, nchar(phenames(spal))))
 
-# marker index within lod curves
-map <- pull.map(spal)
-outspl <- split(cbind(as.data.frame(out), index=(1:nrow(out))-1), out[,1])
-marindex <- map
-for(i in seq(along=map)) {
-  marindex[[i]] <- match(names(map[[i]]), rownames(outspl[[i]]))-1
-  names(marindex[[i]]) <- names(map[[i]])
-}
-# marker anmes
-markers <- lapply(marindex, names)
-
 # genetic map at which LOD scores were calculated
 map <- lapply(spal$geno, function(a) attr(a$prob, "map"))
 for(i in seq(along=map)) {
@@ -34,11 +23,13 @@ for(i in seq(along=map)) {
 }
 
 # pseudomarker indices, for even spacing (every cM)
-evenpmark <- lapply(outspl, function(a) a[match(seq(0, max(a[,2]), by=1), a[,2]),ncol(a),drop=FALSE])
-evenpmark <- lapply(evenpmark, function(a) { b <- unlist(a); names(b) <- rownames(a); b})
+evenpmarindex <- lapply(outspl, function(a) a[match(seq(0, max(a[,2]), by=1), a[,2]),ncol(a),drop=FALSE])
+evenpmarindex <- lapply(evenpmarindex, function(a) { b <- unlist(a); names(b) <- rownames(a); b})
+evenpmar <- unlist(lapply(evenpmarindex, names))
+names(evenpmar) <- NULL
 
 # imputed genotypes at evenly spaced pseudomarkers
-gi <- pull.draws(sim.geno(spal, err=0.002, map.function="kosambi", n.draws=1, step=1))[,unlist(evenpmark)+1,1]
+gi <- pull.draws(sim.geno(spal, err=0.002, map.function="kosambi", n.draws=1, step=1))[,unlist(evenpmarindex)+1,1]
 
 # lod scores
 lod <- as.matrix(out[-(1:2)])
@@ -60,10 +51,9 @@ cat0 <- function(...) cat(..., sep="", file="../Data/spalding_all_lod.json")
 cat0a <- function(...) cat(..., sep="", file="../Data/spalding_all_lod.json", append=TRUE)
 cat0("{\n")
 cat0a("\"chr\" :\n", toJSON(chrnames(spal)), ",\n\n")
-cat0a("\"map\" :\n", toJSON(map), ",\n\n")
-cat0a("\"marindex\" :\n", toJSON(marindex), ",\n\n")
-cat0a("\"markers\" :\n", toJSON(markers), ",\n\n")
-cat0a("\"evenpmark\" :\n", toJSON(evenpmark), ",\n\n")
+cat0a("\"map\" :\n", toJSON(map, digits=10), ",\n\n")
+cat0a("\"evenpmar\" :\n", toJSON(evenpmar), ",\n\n")
+cat0a("\"evenpmarindex\" :\n", toJSON(evenpmarindex), ",\n\n")
 cat0a("\"times\" :\n", toJSON(times), ",\n\n")
 cat0a("\"lod\" :\n", toJSON(lod), ",\n\n")
 cat0a("\"ave1\" :\n", toJSON(ave1), ",\n\n")
